dde5a253b6dd0fa879d36e7282cc48fd0ad34bda
Ishige99 <ishiyamaosamu@icloud.com>
2021-03-01 15:54:47 +0900

    viewの整理・遷移設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :surname, :first_name, :surname_kana, :first_name_kana, :sex_id, :birthday])
+  end
+
 end
