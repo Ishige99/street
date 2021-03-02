ac45a3dcaf1e981e85739deeabaf8018232b96ba
Ishige99 <ishiyamaosamu@icloud.com>
2021-03-02 11:25:08 +0900

    ルーティング・バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }, :length => { :minimum => 6 }
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do 
+      validates :surname
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do 
+      validates :surname_kana
+      validates :first_name_kana
+    end
+    validates :sex_id, includion:{in: [true, false]}
+    validates :birthday
+  end
+
 end
