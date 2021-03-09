e1efe41f1cd360d84db045383d4e1a85460d9f65
Ishige99 <ishiyamaosamu@icloud.com>
2021-03-09 15:33:24 +0900

    テストコードの実行

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :purchase do
-    
+    postal_code { "111-1111" }
+    prefecture { 10 }
+    address = Gimei.address
+    city { address.prefecture.kanji }
+    address_1 { address.city.kanji }
+    address_2 { "マンション101" }
+    phone_number { "00000000000" }
+    token { "tok_abcdefghijk00000000000000000" }
   end
 end
