c09dddafa5636945d5666a3e5571f7381b58964b
Ishige99 <ishiyamaosamu@icloud.com>
2021-03-13 09:47:13 +0900

    コントローラーの記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.create(comment_params)
+    if @comment.valid?
+      @comment.save
+    end
+    redirect_to "/clothes/#{comment.clothe.id}"
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, clothe_id: params[:clothe_id])
+  end
+
 end
