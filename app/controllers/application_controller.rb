029ce4e95e727642796a1be46ae50cf17b83e585
Ishige99 <ishiyamaosamu@icloud.com>
2021-03-14 11:10:07 +0900

    ユーザー情報編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
+    devise_parameter_sanitizer.permit(:sign_up,  
+                                      keys: [:nickname, :surname, :first_name, :surname_kana, :first_name_kana, :sex_id,
+                                             :birthday])
+    devise_parameter_sanitizer.permit(:account_update,  
                                       keys: [:nickname, :surname, :first_name, :surname_kana, :first_name_kana, :sex_id,
                                              :birthday])
   end
