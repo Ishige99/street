c321547324a22def9263c57d3746fa49f0aa785f
Ishige99 <ishiyamaosamu@icloud.com>
2021-03-14 15:06:59 +0900

    テストコード変更

--- spec/models/clothe_spec.rb
+++ spec/models/clothe_spec.rb
@@ -16,82 +16,82 @@ RSpec.describe Clothe, type: :model do
       it 'imageが空' do
         @clothe.image = nil
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include("Image can't be blank")
+        expect(@clothe.errors.full_messages).to include("商品画像を入力してください")
       end
       it 'nameが空' do
         @clothe.name = ''
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include("Name can't be blank")
+        expect(@clothe.errors.full_messages).to include("商品名を入力してください")
       end
       it 'textが空' do
         @clothe.text = ''
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include("Text can't be blank")
+        expect(@clothe.errors.full_messages).to include("商品の説明を入力してください")
       end
       it 'categoryが空' do
         @clothe.category_id = 1
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include('Category must be other than 1')
+        expect(@clothe.errors.full_messages).to include('カテゴリーを入力してください')
       end
       it 'sizeが空' do
         @clothe.size_id = 1
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include('Size must be other than 1')
+        expect(@clothe.errors.full_messages).to include('サイズを入力してください')
       end
       it 'stateが空' do
         @clothe.state_id = 1
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include('State must be other than 1')
+        expect(@clothe.errors.full_messages).to include('商品の状態を入力してください')
       end
       it 'delivery_burdenが空' do
         @clothe.delivery_burden_id = 1
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include('Delivery burden must be other than 1')
+        expect(@clothe.errors.full_messages).to include('配送料の負担を入力してください')
       end
       it 'delivery_methodが空' do
         @clothe.delivery_method_id = 1
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include('Delivery method must be other than 1')
+        expect(@clothe.errors.full_messages).to include('配送方法を入力してください')
       end
       it 'delivery_sourceが空' do
         @clothe.delivery_source_id = 1
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include('Delivery source must be other than 1')
+        expect(@clothe.errors.full_messages).to include('発送元の地域を入力してください')
       end
       it 'delivery_dayが空' do
         @clothe.delivery_day_id = 1
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include('Delivery day must be other than 1')
+        expect(@clothe.errors.full_messages).to include('発送までの日数を入力してください')
       end
       it 'priceが空' do
         @clothe.price = ''
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include("Price can't be blank")
+        expect(@clothe.errors.full_messages).to include("販売価格を入力してください", "販売価格は不正な値です", "販売価格は数値で入力してください")
       end
       it 'priceが300円未満' do
         @clothe.price = '299'
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include('Price must be greater than or equal to 300')
+        expect(@clothe.errors.full_messages).to include('販売価格は300以上の値にしてください')
       end
       it 'priceが9,999,999円より大きい' do
         @clothe.price = '10000000'
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include('Price must be less than or equal to 9999999')
+        expect(@clothe.errors.full_messages).to include('販売価格は9999999以下の値にしてください')
       end
       it 'priceが全角数字' do
         @clothe.price = '１００００'
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include('Price is not a number')
+        expect(@clothe.errors.full_messages).to include('販売価格は数値で入力してください')
       end
       it 'priceが半角英数字混合' do
         @clothe.price = '1000aaa'
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include('Price is not a number')
+        expect(@clothe.errors.full_messages).to include('販売価格は数値で入力してください')
       end
       it 'priceが半角英語' do
         @clothe.price = 'aaaaaa'
         @clothe.valid?
-        expect(@clothe.errors.full_messages).to include('Price is not a number')
+        expect(@clothe.errors.full_messages).to include('販売価格は数値で入力してください')
       end
     end
   end
